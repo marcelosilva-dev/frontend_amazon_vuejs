b7a1603e86cd252ca83475a58d20c4c495f67f8a
marcelosilva-dev <marceloeduardo244@gmail.com>
2022-02-19 11:48:43 -0300

    criada parte visual do componente Buy Product

--- src/pages/BuyProduct.vue
+++ src/pages/BuyProduct.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="login">
+  <NavBar />
+  <div class="buyProduct">
     <h1>BuyProduct</h1>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
+
 export default {
   name: "BuyProduct",
+  components: {
+    NavBar,
+  },
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Welcome to Your Vue.js App",
     };
-  }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
+.sidebar:hover {
+  color: #e47911;
+}
+@media screen and (max-width: 992px) {
+  .side {
+    display: none;
+  }
 }
 </style>
